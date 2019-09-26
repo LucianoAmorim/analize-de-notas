9d9d7b53ddf92e0de609d5e5abdb52ac9b63b3ac
LucianoAmorim <54670022+LucianoAmorim@users.noreply.github.com>
2019-09-25 23:57:14 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int Codigo;
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL, "Portuguese");
+	printf("Notas");
+	printf("Insira seu Código:");
+	scanf("%d", &Codigo);
+	switch(Codigo){
+	
+		case 1:
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+			
+		case 2:
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+			
+		case 3:
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+		
+		case 4:
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+	
+		case 5:
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+			
+		case 6:
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+	
+		case 7:
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+			
+		case 8:
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+			
+		case 9:	
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+			
+		case 10:
+			printf("Código: \n");
+			printf("Nome: \n");
+			printf("Nota de Prova Teoria: \n");
+			printf("Nota de Prova Pratica: \n");
+			printf("Nota de Caderno: \n");
+			printf("Nota de Participação: \n");
+			printf("Nota Final: \n");
+			system("pause");
+			break;
+	
+		default:
+			printf("Código Incorreto");		
+}
+	return 0;
+}
